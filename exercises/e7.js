671db693dc784a2c5ba5967639a7758e840fbf08
Charles Stauffer <cstauff@Charless-MacBook-Pro.local>
2025-06-27 09:51:05 -0700

    local

--- exercises/e7.js
+++ exercises/e7.js
@@ -4,13 +4,22 @@
 // Array example: bankAccounts in /data/data.js
 // getClientWithLeastBalance(bankAccounts) => [{ name: 'SomeName', balance: 32, ... }]
 
-export function getClientWithLeastPositiveBalance(array) {
-  const positiveBalances = array.filter(account => account.balance > 0);
+export const getClientWithLeastPositiveBalance = (array) => {
+  const positiveBalances = array.filter((account) => account.balance > 0);
   if (positiveBalances.length === 0) return [];
 
-  const minBalance = Math.min(...positiveBalances.map(account => account.balance));
-  return positiveBalances.filter(account => account.balance === minBalance);
-}
+  const minBalance = Math.min(
+    ...positiveBalances.map((account) => account.balance)
+  );
+  return positiveBalances.filter((account) => account.balance === minBalance);
+};
+
+// getClientWithLeastPositiveBalance = (array) => {
+//   const positiveBalances = array.filter((account) => account.balance > 0);
+//   positiveBalances.forEach((account) => console.log(account));
+
+//   if (positiveBalances.length === 0) return [];
+// };
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-7"
